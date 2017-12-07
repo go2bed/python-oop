37348c91d6c05c76c560d2bc3a81ac7d503372e2
Andrey_Chadov <androusj@mail.ru>
2017-12-07 21:37:42 +0600

    initial commit

---
+++ oop/Main.py
@@ -0,0 +1,30 @@
+from oop.Book import Book
+
+
+class Dog(object):
+    species = 'mammal'
+
+    def __init__(self, breed, name):
+        self.breed = breed
+        self.name = name
+
+    def area(self):
+        return self.species
+
+    def set_name(self, name):
+        self.name = name
+
+    def get_name(self):
+        return self.name
+
+
+sam = Dog('Lab', 'Sam')
+print(sam.breed)
+
+sam.set_name('bublik')
+print(sam.get_name())
+
+
+
+book = Book("Python Rocks!", "Jose Portilla", 159)
+print(book)
