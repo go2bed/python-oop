34466730256be4d3231b9cab6a672e1ccae496b8
Andrey_Chadov <androusj@mail.ru>
2017-12-08 08:17:00 +0600

    [IMP] Some examples

--- oop/Sample.py
+++ oop/Sample.py
@@ -12,11 +12,14 @@ print(type(x))
 class Dog(object):
     # Class object Atribute
     species = 'mammal'
+    ancestor = 'wolf'
 
-    def __init__(self, breed, name, fur=True):
+    def __init__(self, breed, name, fur=True, paws=4, eyes=2):
         self.breed = breed
         self.name = name
         self.fur = fur
+        self.paws = paws
+        self.eyes = eyes
 
     def __hash__(self) -> int:
         return super().__hash__()
@@ -29,3 +32,4 @@ print(sam.__hash__())
 print(sam.species)
 print(sam.name)
 print(sam.fur)
+print(sam.ancestor)
