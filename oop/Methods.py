63ff8e6715809f21b3aeecd9eb3d6e176bf208b1
Andrey_Chadov <androusj@mail.ru>
2017-12-09 14:04:31 +0600

    [IMP] Methods

---
+++ oop/Methods.py
@@ -0,0 +1,29 @@
+class Circle(object):
+    # class object attributes
+    pi = 3.14
+
+    def __init__(self, radius=1, perimeter=25):
+        self.radius = radius
+        self.perimeter = perimeter
+
+    def area(self):
+        # radius**2 * pi
+        return (self.radius ** 2) * Circle.pi
+
+    def set_radius(self, new_radius):
+        self.radius = new_radius
+
+    def get_radius(self):
+        return self.radius
+
+    def get_perimeter(self):
+        # perimeter =  2 * pi * radius
+        return 2 * Circle.pi * self.radius
+
+
+c = Circle(radius=100)
+c.set_radius(50)
+
+print(c.get_radius())
+print(c.area())
+print(str(c.get_perimeter()) + ' this is a perimeter of the Circle')
